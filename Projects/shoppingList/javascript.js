38984c521ba1190d70eaadd11b97306c094bcc55
svaronamadrid <sergio.varona.madrid@outlook.com>
2021-02-19 22:50:21 +0100

    added local persistance and capacity to delete items from list

--- Projects/shoppingList/javascript.js
+++ Projects/shoppingList/javascript.js
@@ -1,6 +1,25 @@
-const listaCompra=[];
+const listaCompra=JSON.parse(localStorage.getItem('listaCompra')) || [] ;
 
+const synchronize = () =>{
+    const lista=document.getElementById('list');
+    const listaCompraTemplate=listaCompra.map(t => '<li>' +t +'</li>');
+    lista.innerHTML=listaCompraTemplate.join('');
+    const elements=document.querySelectorAll('#list li');
+    elements.forEach((element,i) => {
+        element.addEventListener('click', () =>{
+            element.parentNode.removeChild(element);
+            listaCompra.splice(i,1);
+            updateList(listaCompra);
+            synchronize();
+        })
+    })
+}
+const updateList=(listaCompra) =>{
+    const listToString= JSON.stringify(listaCompra);
+    localStorage.setItem('listaCompra',listToString);
+}
 window.onload=()=>{
+    synchronize();
     const form= document.getElementById('todo-form');
     form.onsubmit=(e)=>{
         e.preventDefault();
@@ -8,9 +27,8 @@ window.onload=()=>{
         const todoText=todo.value;
         todo.value='';
         listaCompra.push(todoText);
-        const lista=document.getElementById('list');
-        const listaCompraTemplate=listaCompra.map(t => '<li>' +t +'</li>');
-        lista.innerHTML=listaCompraTemplate.join('');
-
+        updateList(listaCompra);
+        synchronize();
     }
 }
+
